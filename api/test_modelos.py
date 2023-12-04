3b0778cee989a2fd6a76a984a2789bdda4b1ca73
Leandro Lemes Vilela <leandrolvilela@gmail.com>
2023-12-04 20:29:13 -0300

    Ajustes gerais

--- api/test_modelos.py
+++ api/test_modelos.py
@@ -22,7 +22,7 @@ Y = dataset.iloc[:, -1]
 def test_modelo_lr():  
     # Importando o modelo de regressão logística
     lr_path = 'ml_model/diabetes_lr.pkl'
-    modelo_lr = Model.carrega_modelo(lr_path)
+    modelo_lr = modelo.carrega_modelo(lr_path)
 
     # Obtendo as métricas da Regressão Logística
     acuracia_lr, recall_lr, precisao_lr, f1_lr = avaliador.avaliar(modelo_lr, X, Y)
